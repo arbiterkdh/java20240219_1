dfa96bfed335e58ad86b86ee02761fce490ac4d9
dhkang <arbiterkdh.gmail.com>
2024-02-19 12:41:10 +0900

    처음 만들어본 자바 코드

---
+++ src/Main3.java
@@ -0,0 +1,11 @@
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("123123");
+        System.out.println("234234");
+        System.out.println("345345");
+        System.out.println("456456");
+        System.out.println("567567");
+        System.out.println("789789");
+
+    }
+}
