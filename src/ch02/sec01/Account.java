6040246485c9df211317d051ce800cecf13fed31
dhkang <arbiterkdh.gmail.com>
2024-02-28 17:40:54 +0900

    6장 클래스 19번 문제 풀이

---
+++ src/ch02/sec01/Account.java
@@ -0,0 +1,24 @@
+package ch02.sec01;
+
+public class Account {
+    private int balance;
+    static final int MIN_BALANCE = 0;
+    static final int MAX_BALANCE = 1_000_000;
+
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+        if (this.balance < MIN_BALANCE) {
+            this.balance = MIN_BALANCE;
+        } else if (this.balance > MAX_BALANCE) {
+            this.balance = MAX_BALANCE;
+        }
+
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+
+}
---
+++ src/ch02/sec01/Check.java
@@ -0,0 +1,13 @@
+package ch02.sec01;
+
+public class Check {
+    public static void main(String[] args) {
+        Account account = new Account();
+
+        account.setBalance(100_000);
+        System.out.println("현재 잔고: " + account.getBalance());
+
+        account.setBalance(-100);
+        System.out.println("현재 잔고: " + account.getBalance());
+    }
+}
