0d251c9881f165ed289160160864340400951071
arbiterkdh <arbiterkdh@gmail.com>
2024-03-05 20:29:29 +0900

    7장 상속 예제 풀이

---
+++ src/ch07/sec12/problem12/Example.java
@@ -0,0 +1,16 @@
+package ch07.sec12.problem12;
+
+public class Example {
+    public static void action(A a) {
+        if (a instanceof C c) {
+            c.method2();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        action(new A());
+        action(new B());
+        action(new C());
+    }
+}
