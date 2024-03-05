a3ac92e909056e7760dd4c7a487b45cc48fd50d2
dhkang <arbiterkdh.gmail.com>
2024-03-05 10:01:59 +0900

    7장 상속 SmartPhoneExample 예제3

---
+++ src/ch07/sec03/exam02/SmartPhoneExample.java
@@ -0,0 +1,9 @@
+package ch07.sec03.exam02;
+
+public class SmartPhoneExample {
+    public static void main(String[] args) {
+        SmartPhone smartPhone = new SmartPhone("GalaxyS21", "Navy");
+        System.out.println("모델: " + smartPhone.model);
+        System.out.println("컬러: " + smartPhone.color);
+    }
+}
