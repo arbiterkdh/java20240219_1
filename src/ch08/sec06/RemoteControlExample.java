08242aa55fde198c3387fbaaa7cf5d7f777eb62c
arbiterkdh <arbiterkdh@gmail.com>
2024-03-06 11:58:53 +0900

    8장 인터페이스 예제 풀이

--- src/ch08/sec06/RemoteControlExample.java
+++ src/ch08/sec06/RemoteControlExample.java
@@ -1,22 +1,24 @@
-package ch08.sec05;
+package ch08.sec06;
 
-import ch07.sec12.problem09.A;
 
 public class RemoteControlExample {
-
     public static void main(String[] args) {
         RemoteControl rc = new Television();
         rc.turnOn();
         rc.setVolume(5);
+
         rc.setMute(true);
         rc.setMute(false);
+
         System.out.println();
+
         rc = new Audio();
         rc.turnOn();
         rc.setVolume(5);
 
         rc.setMute(true);
         rc.setMute(false);
-        rc.turnOff();
+        System.out.println();
+        RemoteControl.changeBattery();
     }
 }
