cea5e30c38a0fffb34967873d3d72dd9742258b0
arbiterkdh <arbiterkdh@gmail.com>
2024-03-06 17:17:31 +0900

    8장 인터페이스 확인 문제 풀이

---
+++ src/ch08/sec14/problem07/OracleDao.java
@@ -0,0 +1,19 @@
+package ch08.sec14.problem07;
+
+public class OracleDao implements DataAccessObject {
+    public void select() {
+        System.out.println("Oracle DB 에서 검색");
+    }
+
+    public void insert() {
+        System.out.println("Oracle DB 에 삽입");
+    }
+
+    public void update() {
+        System.out.println("Oracle DB 를 수정");
+    }
+
+    public void delete() {
+        System.out.println("Oracle DB 에서 삭제");
+    }
+}
