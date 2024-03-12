3e7a71df618f6d2bab7863ae4f5a00df9a7de2eb
arbiterkdh <arbiterkdh@gmail.com>
2024-03-12 10:12:45 +0900

    이것이 자바다 9장 수업

--- src/ch08/sec14/test/test5.java
+++ src/ch08/sec14/test/test5.java
@@ -1,20 +1,18 @@
 package ch08.sec14.test;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class test5 {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
-        int[] delete_list = {1, 2, 3};
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int e : arr) {
-            list.add(e);
+        String a = "qweqwe";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < a.length(); i++) {
+            sb.append(a.charAt(i));
         }
-        for (int e : delete_list) {
-            list.remove((Integer) e);
-        }
-        System.out.println(Arrays.toString(list.toArray()));
+        sb.reverse();
+        System.out.println(sb.toString());
     }
 }
