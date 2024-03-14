36408e104825f36c272f7eadad7cd37fc6f9414e
arbiterkdh <arbiterkdh@gmail.com>
2024-03-14 14:13:48 +0900

    이것이 자바다 12장 수업

---
+++ src/ch12/lecture/p1object/C03Equals.java
@@ -0,0 +1,11 @@
+package ch12.lecture.p1object;
+
+public class C03Equals {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 3;
+        Object a = i;
+        Object b = j;
+        System.out.println(a.equals(b));
+    }
+}
---
+++ src/ch12/sec13/problem06/Test.java
@@ -0,0 +1,7 @@
+package ch12.sec13.problem06;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
