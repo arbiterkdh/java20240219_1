2630d8baf692a0c75b51b6598ef042924beb358a
arbiterkdh <arbiterkdh@gmail.com>
2024-03-18 23:57:37 +0900

    이것이 자바다 15장 수업

--- src/ch15/sec09/problem07/test.java
+++ src/ch15/sec09/problem07/test.java
@@ -1,13 +1,47 @@
 package ch15.sec09.problem07;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        List<List<Integer>> answer = new ArrayList<>();
-        answer.add(new ArrayList<>() {
-        });
-        answer.get(0).add(1);
+
+        Solution solution = new Solution();
+        System.out.println(solution.solution(6, new int[]{1, 3, 2, 5, 4, 5, 2, 3}));
+
     }
 }
+
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(tangerine);
+
+        for (int i = 0; i < tangerine.length - 1; i++) {
+            if (tangerine[i] == 0) continue;
+
+            int cnt = 1;
+
+            for (int j = i + 1; j < tangerine.length; j++) {
+                if (tangerine[i] == tangerine[j]) {
+                    cnt++;
+                    tangerine[j] = 0;
+                } else {
+                    break;
+                }
+            }
+            list.add(cnt);
+        }
+        Collections.sort(list);
+
+        int answer = 0;
+        while (k > 0) {
+            k -= list.getLast();
+            list.removeLast();
+            answer++;
+        }
+
+        return answer;
+    }
+}
+
