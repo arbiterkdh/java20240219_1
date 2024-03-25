63049776872c9ac00d0c728a6717c5025bb73603
arbiterkdh <arbiterkdh@gmail.com>
2024-03-25 20:57:10 +0900

    이것이 자바다 11장 수업

---
+++ src/programmers/problem08/Example.java
@@ -0,0 +1,15 @@
+package programmers.problem08;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example {
+    public static void main(String[] args) {
+
+
+        int[][] answer = new int[][]{{1, 2, 3}, {4, 5, 6}};
+
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        List<int[]> answer1 = List.of(answer);
+    }
+}
