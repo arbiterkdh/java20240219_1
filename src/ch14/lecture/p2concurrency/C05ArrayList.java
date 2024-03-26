1726219f1cc8edfdaecb7c533662ae1eef1dc228
arbiterkdh <arbiterkdh@gmail.com>
2024-03-26 15:17:20 +0900

    이것이 자바다 14장 수업

---
+++ src/ch14/lecture/p2concurrency/C05ArrayList.java
@@ -0,0 +1,33 @@
+package ch14.lecture.p2concurrency;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class C05ArrayList {
+    public static void main(String[] args) throws InterruptedException {
+        List<String> vector = new Vector<>();
+
+        Thread t1 = new Thread(() ->
+        {
+            for (int i = 0; i < 30000; i++) {
+                vector.add("a");
+                vector.remove("a");
+            }
+        });
+        Thread t2 = new Thread(() ->
+        {
+            for (int i = 0; i < 30000; i++) {
+                vector.add("b");
+                vector.remove("b");
+            }
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println(vector);
+
+    }
+}
