909d10dce0bf3d0997009b03237701c1f4aafbf0
arbiterkdh <arbiterkdh@gmail.com>
2024-03-26 12:38:15 +0900

    이것이 자바다 14장 수업

--- src/ch14/lecture/p1thread/C08Thread.java
+++ src/ch14/lecture/p1thread/C08Thread.java
@@ -1,4 +1,21 @@
 package ch14.lecture.p1thread;
 
 public class C08Thread {
+    public static void main(String[] args) {
+        Thread t = new Thread(() -> {
+            for (int i = 0; i < 30000; i++) {
+                System.out.println(i);
+            }
+        });
+
+        t.start();
+        try {
+            t.join(); // 현재 쓰레드가 t 쓰레드의 종료를 기다림
+        } catch (Exception e) {
+        }
+
+        for (int i = 0; i < 30000; i++) {
+            System.err.println(i);
+        }
+    }
 }
