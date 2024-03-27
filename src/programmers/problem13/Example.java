0aaef6fff650e50adccdeac1b256235ca44cc12f
arbiterkdh <arbiterkdh@gmail.com>
2024-03-27 19:40:38 +0900

    이것이 자바다 18장 수업

---
+++ src/programmers/problem13/Example.java
@@ -0,0 +1,21 @@
+package programmers.problem13;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Example {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        String[] friends = {"a", "b", "c"};
+        String[] gifts = {"a b", " b a", "a c", "c a", "b c", "b c"};
+        for (String f : friends) {
+            map.put(f, 0);
+        }
+        for (String gift : gifts) {
+            String[] g = gift.split(" ");
+            Integer give = map.get(g[0]) + 1;
+            Integer take = map.
+                    map.put(g[0], n);
+        }
+    }
+}
