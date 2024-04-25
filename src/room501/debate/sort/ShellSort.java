333c3e13f17178df637d876ed73f44d23c11baba
arbiterkdh <arbiterkdh@gmail.com>
2024-04-25 15:54:29 +0900

    정렬법 예제

--- src/room501/debate/sort/ShellSort.java
+++ src/room501/debate/sort/ShellSort.java
@@ -12,10 +12,8 @@ public class ShellSort {
         // 임의의 순서로 나열된 배열 할당.
         int swap; // 조건에 맞으면 swap 하기 위한 변수 생성
         // 셸정렬은 gap 을 이용해서 배열을 부분정렬하고,
-        // 매 반복마다 gap 을 반씩 줄여나가는 게 정석이나,
-        // 여러 자료에서 말하길, gap 의 간격을 gap / 3 + 1 로
-        // 하는 것이 더 효율적이라고 합니다.
-        for (int gap = arr.length / 3 + 1; gap > 0; gap /= 3 + 1) {
+        // 매 반복마다 gap 을 반씩 줄여나가며 정렬을 수행합니다.
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
             // 매 반복시의 갭 간격 설정
             for (int i = 0; i < arr.length - gap; i++) {
                 // 각 반복에서 필요한 만큼만 순환하게끔 범위 설정
